064cce3aaae69abcb33223822c69e61ebdd858e1
mtleiva <migueltomasleiva@gmail.com>
2023-01-23 14:13:54 -0300

    presentable

---
+++ ValkimiaChallenge/DataService/FacturaService.cs
@@ -0,0 +1,113 @@
+﻿using ValkimiaChallenge.Models;
+using ValkimiaChallenge.Models.DTO;
+
+namespace ValkimiaChallenge.DataService
+{
+    public class FacturaService
+    {
+        private FacturasContext _context { get; set; }
+
+
+        public FacturaService(FacturasContext context) { 
+            this._context = context;
+        }
+        
+
+        public Factura? GetFactura(int id) {
+            try
+            {
+                var result = _context.Facturas.Where(
+                    factura => (factura.Id == id)).FirstOrDefault();
+                return result;
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+
+        public List<Factura> GetFacturas()
+        {
+            try
+            {
+                var result = _context.Facturas.ToList();
+                return result;
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+
+        public List<Factura> GetFacturas(int clienteId)
+        {
+            try
+            {
+                var result = _context.Facturas.Where(factura => factura.ClienteId == clienteId).ToList();
+                return result;
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+
+        public void SetFactura(FacturaDTO facturaDTO)
+        {
+            try
+            {
+
+                Factura newFactura = new Factura();
+                newFactura.Fecha = DateTime.Now ;
+                newFactura.Detalle = facturaDTO.Detalle ;
+                newFactura.Importe = facturaDTO.Importe ;
+                newFactura.ClienteId = facturaDTO.ClienteId ;
+                _context.Add(newFactura);
+                _context.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new ApplicationException("Error cargando Factura");
+            }
+
+        }
+
+        public bool DeleteFactura(int id)
+        {
+            try
+            {
+                Factura factura = _context.Facturas.Where(
+                    factura => (factura.Id == id)).FirstOrDefault();
+
+                _context.Remove(factura);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+
+
+        //public Factura GetFacturaModel(FacturaDTO facturaDTO)
+        //{
+        //    Factura newFactura = new Factura();
+        //    newFactura.Nombre = facturaDTO.Nombre;
+        //    newFactura.Apellido = facturaDTO.Apellido;
+        //    newFactura.Domicilio = facturaDTO.Domicilio;
+        //    newFactura.Email = facturaDTO.Email;
+        //    newFactura.Password = facturaDTO.Password;
+        //    newFactura.CuidadId = facturaDTO.CuidadId;
+        //    return newFactura;
+        //}
+
+
+    }
+}
---
+++ ValkimiaChallenge/Models/DTO/FacturaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ValkimiaChallenge.Models.DTO
+{
+    public class FacturaDTO
+    {
+        public int? Id { get; set; }
+        public DateTime? Fecha { get; set; }
+        public string Detalle { get; set; }
+        public decimal Importe { get; set; }
+        public int ClienteId { get; set; }
+    }
+}
